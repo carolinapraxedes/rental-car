4088b670c96243057f86196c780e341948221ce6
CarolinaPraxedes <ana_carolina47@hotmail.com>
2024-09-10 21:15:01 -0300

    feat: migrations,controllers e models de brand,locations,vehicle,etc

---
+++ app/Http/Controllers/VehicleModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle_model;
+use Illuminate\Http\Request;
+
+class VehicleModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Vehicle_model  $vehicle_model
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vehicle_model $vehicle_model)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Vehicle_model  $vehicle_model
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vehicle_model $vehicle_model)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Vehicle_model  $vehicle_model
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vehicle_model $vehicle_model)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Vehicle_model  $vehicle_model
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vehicle_model $vehicle_model)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_09_10_234404_create_vehicles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehicle_model_id');
+            $table->string('license_plate',10)->unique();
+            $table->boolean('available');
+            $table->integer('km');
+            $table->timestamps();
+
+            $table->foreign('vehicle_model_id')->references('id')->on('vehicle_models');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
